76034f683ea46085b49bec7fcdeab4b74bf5a810
Shajadulhaqueemon111 <mdshajdulhaqueemon@gmail.com>
2025-08-21 16:41:11 +0600

    add professional progress bar

--- src/components/pages/form-steps/ProgressBar/ProgressBar.tsx
+++ src/components/pages/form-steps/ProgressBar/ProgressBar.tsx
@@ -1,6 +1,6 @@
 type ModernProgressBarProps = {
   currentStep: number;
-  steps: string[]; // step names
+  steps: string[];
 };
 
 export default function ProgressBar({
@@ -8,42 +8,15 @@ export default function ProgressBar({
   steps,
 }: ModernProgressBarProps) {
   return (
-    <div className="flex justify-between items-center w-full">
-      {steps.map((step, index) => {
-        const isCompleted = index < currentStep;
-        const isActive = index === currentStep;
-
-        return (
-          <div
-            key={index}
-            className="flex-1 flex flex-col items-center relative"
-          >
-            {index !== 0 && (
-              <div
-                className={`absolute top-1/2 left-1 w-full h-0.5 -translate-x-1/2 bg-gray-300 ${
-                  isCompleted ? "bg-blue-600" : ""
-                }`}
-                style={{ zIndex: 0 }}
-              />
-            )}
-
-            <div
-              className={`w-8 h-8 rounded-full flex items-center justify-center z-10
-    ${
-      isCompleted
-        ? "bg-blue-600 text-white"
-        : isActive
-        ? "border-2 border-blue-600 text-blue-600"
-        : "border-2 border-gray-300 text-gray-400"
-    }`}
-            >
-              {index + 1}
-            </div>
-
-            <span className="mt-2 text-xs text-center">{step}</span>
-          </div>
-        );
-      })}
-    </div>
+    <ul className="steps w-full ">
+      {steps.map((step, index) => (
+        <li
+          key={index}
+          className={`step ${index <= currentStep ? "step-primary" : ""}`}
+        >
+          {step}
+        </li>
+      ))}
+    </ul>
   );
 }
